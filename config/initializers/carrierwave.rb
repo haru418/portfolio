ec7fd3e848587cf15ac176d8ed78c3e6a003840d
haru418 <80524490+haru418@users.noreply.github.com>
2021-09-22 16:01:08 +0900

    carrierwave.rbの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIA3DIJT2LWH7C5F54E',
-    :aws_secret_access_key  => 'VlupHvvYUYn4OAgAj2NevSnE5qkt2gXkKX',
-    :region                 => 'ap-northeast-1'
-  }
-
-  config.fog_directory = 'haru-pf-app' if Rails.env.production?
-  # config.fog_directory = 'your_backet_for_dev' if Rails.env.development?
-
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider              => 'AWS',
+      :region                => ENV['ap-northeast-1'],
+      :aws_access_key_id     => ENV['AKIA3DIJT2LWH7C5F54E'],
+      :aws_secret_access_key => ENV['VlupHvvYUYn4OAgAj2NevSnE5qkt2gXkKX']
+    }
+    config.fog_directory     =  ENV['haru-pf-app']
+  end
 end
