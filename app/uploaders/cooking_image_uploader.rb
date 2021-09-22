675422015093dc01f726aa9f2b1045c4382c7d7e
haru418 <80524490+haru418@users.noreply.github.com>
2021-09-22 18:49:27 +0900

    uploaderの変更

--- app/uploaders/cooking_image_uploader.rb
+++ app/uploaders/cooking_image_uploader.rb
@@ -5,8 +5,8 @@ class CookingImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # S3 storage
-  storage :fog
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
