cc0ea092a12dec9247c531fbd6984ca46664274c
haru418 <80524490+haru418@users.noreply.github.com>
2021-09-22 18:42:45 +0900

    carrierwave.rbの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,12 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
-    config.storage = :fog
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws['AKIA3DIJT2LWH7C5F54E'],
-      aws_secret_access_key: Rails.application.credentials.aws['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'],
-      region: 'ap-northeast-1'
+      :provider              => 'AWS',
+      :region                => ENV['ap-northeast-1'],
+      :aws_access_key_id     => ENV['AKIA3DIJT2LWH7C5F54E'],
+      :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory  = 'haru-pf-app'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3haru-pf-app'
+    config.fog_directory     =  ENV['haru-pf-app']
   end
 end
