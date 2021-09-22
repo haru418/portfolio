d55343c6a71317349bd25eb89cfbcea24e566c85
haru418 <80524490+haru418@users.noreply.github.com>
2021-09-22 15:31:43 +0900

    carrierwave.rbの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIA3DIJT2LWH7C5F54E',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => AKIA3DIJT2LWH7C5F54E,
+    :aws_secret_access_key  => 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,
     :region                 => 'ap-northeast-1'
   }
 
