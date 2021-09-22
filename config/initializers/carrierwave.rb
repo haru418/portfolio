eca7db70a733c858c88a84e143b52fe792ace2c6
haru418 <80524490+haru418@users.noreply.github.com>
2021-09-22 16:04:48 +0900

    carrierwave.rbの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],
       :aws_access_key_id     => ENV['AKIA3DIJT2LWH7C5F54E'],
-      :aws_secret_access_key => ENV['VlupHvvYUYn4OAgAj2NevSnE5qkt2gXkKX']
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['haru-pf-app']
   end
